61e05d49ecb6121f19850a52eefce6129ec4e613
alex <31458482+alexinite@users.noreply.github.com>
2022-05-26 01:59:36 -0400

    test: add camera playability

--- src/init.server.lua
+++ src/init.server.lua
@@ -1,5 +1,7 @@
+local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local Keyframe = require(script.Keyframe)
+local Tracing = require(script.Tracing)
 
 -- plugin init
 local toolbar = plugin:CreateToolbar("Cutscene Editor")
@@ -11,17 +13,33 @@ local function getLookingCFrame()
 end
 
 -- handle user inputs
+local tracePlaying = false
+local currentTrace
 local toggle = false
 button.Click:Connect(function()
     toggle = not toggle
 end)
 
 UserInputService.InputBegan:Connect(function(input: InputObject)
-    if not toggle then
+    --[[if not toggle or not RunService:IsEdit() then
         return
-    end
+    end]]
 
-    if input.KeyCode == Enum.KeyCode.K then
-        Keyframe.new(getLookingCFrame())
+    if not RunService:IsEdit() then
+        if input.KeyCode == Enum.KeyCode.P then
+            if not currentTrace then
+                Tracing.new(Keyframe.table, {}, {})
+            end
+            tracePlaying = not tracePlaying
+            if tracePlaying then
+                Tracing:begin()
+            else
+                Tracing:stop()
+            end
+        end
+    else
+        if input.KeyCode == Enum.KeyCode.K then
+            Keyframe.new(getLookingCFrame())
+        end
     end
 end)
